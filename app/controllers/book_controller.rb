db391c794e00daeec35fc402242166d95e2ad0ed
isabelledouin <isabelle.douin@orange.fr>
2021-12-24 17:38:11 +0100

    gestion table Book en mode CRUD

---
+++ app/controllers/book_controller.rb
@@ -0,0 +1,47 @@
+class BookController < ApplicationController
+
+  def index
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new
+    if @book.save
+      redirect_to books_path(@book)
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @book.update
+      redirect_to books_path
+    else
+      render :edit
+   end
+  end
+
+  def destroy
+    @book.destroy
+    reidrect_to books_path
+  end
+
+private
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :author, :year, :resume, :price)
+  end
+end
